d4c042455ce814333a3d0a25edb5dc512aabb57d
MarianaMPC <mariana13mpc@gmail.com>
2020-12-13 21:45:57 -0300

    1

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,7 +17,7 @@ function Home() {
         <Button><Link to="/sign-up">Sign Up</Link></Button>
         <h3>Quem somos ?</h3>
         <p>Nosso grupo é composto pelos estudantes Mariana Martins Pessoa Costa, Phelipe Resende e Natália. Para nos avaliar tem o projeto do github e o vídeo do youtube.</p>
-        <Button><Link to="https://github.com/PhelipeSR/youme-backend/blob/main/package.json">GitHub</Link></Button>
+        <Button><Link to="https://github.com/marianampc/Trabalho_Final-Topicos3">GitHub</Link></Button>
         <Button><Link to="/sign-up">Youtube</Link></Button>
       </Row>
       <Footer />
